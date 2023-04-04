e7ccaff0aee9f7e5f7b3bc2963323538708fa2b9
MadhurPatidarHVT <madhur.patidar@hexaviewtech.com>
2023-04-04 13:07:13 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { Component, Fragment } from "react";
+import Header from "./components/Header";
+import Home from "./components/home";
 
-function App() {
-  const [data, setData] = useState([]);
-    useEffect(() => {
-   fetch('/app/api/')
-   .then(response => setData(response))
-  //  .then(data => console.log(data))
-
-   .catch(error => console.error(error));
- }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-          {data?.data}
-          
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <Home />
+      </Fragment>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
 
+export const API_URL = "http://localhost:8000/api/students/";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
